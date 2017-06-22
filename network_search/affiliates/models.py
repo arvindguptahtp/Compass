969decfe19d551f718b6e6a91c1c78ade7c0a00f
Ben Lopatin <ben@benlopatin.com>
2017-06-22 12:35:15 +0200

    Add base data for affiliates and EOY data

--- network_search/affiliates/models.py
+++ network_search/affiliates/models.py
@@ -99,7 +99,7 @@ class Affiliate(BaseResource):
     official_name = models.CharField(max_length=200)
     address_street = models.CharField(max_length=200)
     address_city = models.CharField(max_length=200)
-    address_state = models.CharField(max_length=2)
+    address_state = models.CharField(max_length=30)
     address_postcode = models.CharField(max_length=10)
     shipping_address = models.CharField(max_length=400, null=True, blank=True)
     phone_number = models.CharField(max_length=30, null=True)
@@ -122,11 +122,11 @@ class AffiliateEOYData(TimeStampedModel):
     affiliate = models.ForeignKey('Affiliate', related_name='affiliate_eoy_data')
     year = models.ForeignKey('EndOfYear', related_name='affiliate_eoy_data')
 
-    districts = ArrayField(
-        models.CharField(max_length=100, blank=True),
+    districts = models.TextField(
+        max_length=100,
         blank=True,
         null=True,
-        help_text="School district names should be separated by commas."
+        help_text="School district names should be separated by commas.",
     )
 
     search_students_female = models.IntegerField(default=0, editable=False)
--- network_search/affiliates/tasks.py
+++ network_search/affiliates/tasks.py
@@ -1,5 +1,5 @@
 """
-
+Task processing for upload processing
 """
 
 import logging
@@ -11,6 +11,11 @@ from celery import shared_task
 from network_search.affiliates import models
 
 logger = logging.getLogger(__name__)
+
+
+AFFILIATE_SHEET_NAME = 'Affiliate_Data'
+SCHOOL_SHEET_NAME = 'School_Data'
+STUDENT_SHEET_NAME = 'Student_Data'
 
 
 @shared_task
@@ -32,14 +37,82 @@ def process_data_upload(excel_upload_pk):
         return
 
     try:
-        affiliates_list = pd.read_excel(data_upload.data_file, sheetname=0, header=0, skiprows=[0])
+        affiliates_workbook = pd.read_excel(
+            data_upload.data_file,
+            sheetname=None,
+            header=0,
+        )
     except Exception as e:
-        data_upload.status = 'f'
+        data_upload.status = data_upload.FAILED
+        data_upload.message = "{}".format(e)
         data_upload.save()
         logger.exception("Error reading Affiliates file/worksheet")
-        raise
-        # return
+        return
 
-    print("DONE")
+    affiliate_messages = []
+
+    affiliate_data = affiliates_workbook[AFFILIATE_SHEET_NAME]
+    school_data = affiliates_workbook[SCHOOL_SHEET_NAME]
+    student_data = affiliates_workbook[STUDENT_SHEET_NAME]
+    school_student_data = school_data.merge(student_data, on=["Affiliate Name", "School Name"])
+
+    logger.debug("Iterating over dataframe...")
+
+    for index, row in affiliate_data.iterrows():
+        logger.debug("...over row {} now...".format(index))
+        try:
+            affiliate, created = models.Affiliate.affiliates.get_or_create(
+                name=row['Affiliate Name'],
+                defaults={
+                    'state': row['State'],
+                    'cis_id': row['Affiliate ID'],
+                    'official_name': row["Affiliate's Official Name"],
+                    'address_street': row['Address (# and Street)'],
+                    'address_city': row['City'],
+                    'address_state': row['State2'],
+                    'address_postcode': row['Zip'],
+                    'shipping_address': row['Shipping Address'],
+                    'phone_number': row['Phone'],
+                    'fax_number': row['Fax'],
+                    'website': row['Website Address'],
+                    'affiliate_location': row['Affiliate Location'],
+                },
+            )
+            logger.info("{} affiliate '{}'".format("Created" if created else "Found", affiliate))
+        except Exception as e:
+            logger.exception("Error processing data row in affiliate Excel upload")
+            affiliate_messages.append('Error processing row: {}'.format(e))
+        else:
+
+            affiliate_eoy_data, created = models.AffiliateEOYData.objects.get_or_create(
+                affiliate=affiliate,
+                year=data_upload.year,
+                defaults={
+                    'districts': row['Name(s) of district(s) served'],
+                },
+            )
+            logger.info("{} affiliate EOY data '{}'".format("Created" if created else "Found", affiliate_eoy_data))
+
+            affiliate_schools = school_student_data.loc[school_student_data["Affiliate Name"] == affiliate.name]
+
+            logger.info("Found {} schools for affiliate {}".format(len(affiliate_schools), affiliate))
+
+            for index, school_row in affiliate_schools.iterrows():
+                logger.debug("Iterating through school data subsection now")
+                try:
+                    school, created = models.SchoolEOYData.objects.get_or_create(
+                        affiliate_data=affiliate_eoy_data,
+                        name=school_row["School Name"],
+                    )
+                except:
+                    logger.exception("COULD NOT READ OR CREATE SCHOOL")
+                else:
+                    logger.info("{} {} school EOY data '{}'".format(
+                        "Created" if created else "Found", data_upload.year, school))
+
+
+    data_upload.status = data_upload.COMPLETED
+    data_upload.message = '\n'.join(affiliate_messages)
+    data_upload.save()
 
 
