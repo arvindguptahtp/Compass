bbde74bae7e77b66b44f76ea7f96aa07055c6266
Cobey Potter <cobey.potter@wellfireinteractive.com>
2017-06-26 15:02:15 -0400

    updates for initial build

--- front-end/vefa/razor.js
+++ front-end/vefa/razor.js
@@ -1,10 +1,11 @@
 const razor = require("css-razor").default
 const fs = require('fs')
+const yargs = require('yargs').argv
 const yaml = require('js-yaml')
 
 const postcss = require('postcss')
 
-let config = yaml.safeLoad( fs.readFileSync(`${__dirname}/../vefa.yaml`, 'utf8') )
+let config = yaml.safeLoad( fs.readFileSync(`${__dirname}/../${ yargs.config }`, 'utf8') )
 
 let files = config.styles.emit.forEach( (file) => {
     let file_expect = `./${ config.build }${ config.styles.build }${file.replace("styl", "css")}`
