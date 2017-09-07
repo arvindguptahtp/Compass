4992a4a23ac84e57be629e28ab311eeb85ae93bd
Ben Lopatin <ben@benlopatin.com>
2017-09-07 14:46:50 -0400

    Update default allowed domains

--- config/settings/production.py
+++ config/settings/production.py
@@ -60,7 +60,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['network.communitiesinschools.org', 'cis-network-search.herokuapp.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['compass.communitiesinschools.org', 'cis-network-search.herokuapp.com'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
